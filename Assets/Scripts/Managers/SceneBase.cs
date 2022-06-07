a0bbaf680ff9cb439b696532b36ff10efd8d178a
Gcol <colletto.gregory@gmail.com>
2022-06-07 07:29:19 +0200

    Initial commit

---
+++ Assets/Scripts/Managers/SceneBase.cs
@@ -0,0 +1,383 @@
+﻿using Cinemachine;
+using DG.Tweening;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.Rendering.PostProcessing;
+using Utils;
+using Utils.Dependency;
+using static Facade;
+
+public abstract class SceneBase : Singleton<SceneBase>
+{
+	public const string MASTER_VOLUME = "masterVolume";
+	public const string MUSIC_VOLUME = "musicVolume";
+	public const string MUSIC_LOWPASS = "musicLowPass";
+
+	public delegate void SceneEventHandler();
+
+	public event SceneEventHandler OnStart;
+
+	public event SceneEventHandler OnEnd;
+
+	public event SceneEventHandler OnPause;
+
+	[Header("Audio")]
+	[SerializeField] protected AudioClip music;
+
+	[Header("Local References")]
+	[SerializeField] protected Dependency<FadeScreen> _fader;
+	[SerializeField] protected Dependency<CinemachineImpulseSource> _impulse;
+	[SerializeField] protected Dependency<CinemachineVirtualCamera> _camera;
+	[SerializeField] protected Dependency<PostProcessVolume> _volume;
+
+	[Header("Project References")]
+	[SerializeField] protected AudioMixer mixer;
+	[SerializeField] protected Material transition;
+
+	private SceneState state;
+	private float masterVolume;
+	private float musicVolume;
+	private float musicLowPass;
+	private float startOrthographicSize;
+	private bool isMusicMuted;
+	private Coroutine loading;
+	private Coroutine inversingColor;
+	private Coroutine whiteScreen;
+	private FloatParameter startVignetteIntensity;
+	private FloatParameter startChromaticAberation;
+	private Tween zooming;
+	private Tween updatingVignette;
+	private Tween updatingChromatic;
+	private Vignette vignette;
+	private ChromaticAberration chromatic;
+	private Transform defaultCameraTarget;
+
+	public SceneState State
+	{
+		get => state;
+		set
+		{
+			state = value;
+
+			switch (value)
+			{
+				case SceneState.Start:
+					OnStart?.Invoke();
+					break;
+
+				case SceneState.End:
+					OnEnd?.Invoke();
+					break;
+
+				case SceneState.Pause:
+					OnPause?.Invoke();
+					break;
+			}
+		}
+	}
+
+	protected FadeScreen fader => _fader.Resolve(this);
+	protected CinemachineImpulseSource impulse => _impulse.Resolve(this);
+	protected CinemachineVirtualCamera currentCamera => _camera.Resolve(this);
+	protected PostProcessVolume volume => _volume.Resolve(this);
+
+	protected override void Awake()
+	{
+		base.Awake();
+
+		Application.targetFrameRate = 60;
+
+		DOTween.Init();
+		DOTween.defaultAutoPlay = AutoPlay.All;
+
+		// Disable screen dimming
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+	}
+
+	protected virtual void Start()
+	{
+		// Post-Processing
+		ResetShaders();
+		if (volume != null && volume.profile.TryGetSettings<Vignette>(out vignette))
+		{
+			startVignetteIntensity = vignette.intensity;
+		}
+		if (volume != null && volume.profile.TryGetSettings<ChromaticAberration>(out chromatic))
+		{
+			startChromaticAberation = chromatic.intensity;
+		}
+
+		// Audio
+		if (mixer != null)
+		{
+			mixer.GetFloat(MASTER_VOLUME, out masterVolume);
+			mixer.GetFloat(MUSIC_VOLUME, out musicVolume);
+			mixer.GetFloat(MUSIC_LOWPASS, out musicLowPass);
+		}
+		if (Music != null)
+		{
+			Music.TryUpdateClip(music);
+			Music.SwitchBackToMain();
+		}
+
+		// Effects
+		if (currentCamera != null)
+		{
+			startOrthographicSize = currentCamera.m_Lens.OrthographicSize;
+		}
+		if (fader != null)
+		{
+			fader.FadeIn();
+		}
+
+		State = SceneState.Start;
+	}
+
+	protected virtual void Update()
+	{
+#if UNITY_STANDALONE_OSX || UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX || UNITY_EDITOR
+		if (Input.GetButtonDown("Quit"))
+		{
+			ResetShaders();
+			SceneLoader.Quit();
+		}
+		if (Input.GetButtonDown("Mute"))
+		{
+			if (isMusicMuted)
+			{
+				UpdateSceneMasterVolume(1f);
+			}
+			else
+			{
+				UpdateSceneMasterVolume(0f);
+			}
+			isMusicMuted = !isMusicMuted;
+		}
+		if (Input.GetButtonDown("Fullscreen"))
+		{
+			Screen.fullScreen = !Screen.fullScreen;
+		}
+#endif
+	}
+
+	#region Post-Processing and Effects
+
+	public void GenerateImpulse()
+	{
+		impulse.GenerateImpulse();
+	}
+
+	private void ResetShaders()
+	{
+		transition.SetFloat("_isInversed", 0);
+		transition.SetFloat("_forceColor", 0);
+	}
+
+	public void InverseColor(float duration = 0.05f)
+	{
+		if (inversingColor != null)
+			StopCoroutine(inversingColor);
+
+		inversingColor = StartCoroutine(ApplingShader("_isInversed", duration));
+	}
+
+	public void ForceColorScreen(Color color, float duration = 0.05f)
+	{
+		if (whiteScreen != null)
+			StopCoroutine(whiteScreen);
+
+		transition.SetColor("_Color", color);
+		whiteScreen = StartCoroutine(ApplingShader("_forceColor", duration));
+	}
+
+	public void FreezeTime(float duration = 0.1f)
+	{
+		Time.timeScale = 0f;
+		DOTween.To(() => Time.timeScale, x => Time.timeScale = x, 1, duration).SetEase(Ease.OutSine).SetUpdate(true);
+	}
+
+	public void SetCameraTarget(Transform target)
+	{
+		if (defaultCameraTarget == null)
+			defaultCameraTarget = currentCamera.m_Follow;
+
+		currentCamera.m_Follow = target;
+	}
+
+	public void ResetCameraTarget()
+	{
+		currentCamera.m_Follow = defaultCameraTarget;
+	}
+
+	public void Zoom(float value, float duration = 1f, Ease ease = Ease.OutSine)
+	{
+		zooming?.Kill();
+		currentCamera.m_Lens.OrthographicSize = startOrthographicSize;
+		zooming = DOTween.To(() => currentCamera.m_Lens.OrthographicSize, x => currentCamera.m_Lens.OrthographicSize = x, value, duration).SetEase(ease);
+	}
+
+	public void ResetZoom(float duration = 1f, Ease ease = Ease.OutSine)
+	{
+		zooming?.Kill();
+		zooming = DOTween.To(() => currentCamera.m_Lens.OrthographicSize, x => currentCamera.m_Lens.OrthographicSize = x, startOrthographicSize, duration).SetEase(ease);
+	}
+
+	public void SetVignette(float value, float duration, Ease ease)
+	{
+		if (vignette == null)
+		{
+			Debug.LogWarning("Vignette effect has not been initialized. is PostProcessVolume component missing?");
+			return;
+		}
+
+		updatingVignette?.Kill();
+		vignette.intensity = startVignetteIntensity;
+		updatingVignette = DOTween.To(() => vignette.intensity.value, x => vignette.intensity.value = x, value, duration).SetEase(ease).SetLoops(2, LoopType.Yoyo);
+	}
+
+	public void SetChromaticAberation(float value, float duration, Ease ease)
+	{
+		if (chromatic == null)
+		{
+			Debug.LogWarning("Chromatic effect has not been initialized. is PostProcessVolume component missing?");
+			return;
+		}
+
+		updatingChromatic?.Kill();
+		chromatic.intensity = startChromaticAberation;
+		updatingChromatic = DOTween.To(() => chromatic.intensity.value, x => chromatic.intensity.value = x, value, duration).SetEase(ease).SetLoops(2, LoopType.Yoyo);
+	}
+
+	private IEnumerator ApplingShader(string parameter, float duration)
+	{
+		ResetShaders();
+		transition.SetFloat(parameter, 1);
+		yield return new WaitForSeconds(duration);
+		transition.SetFloat(parameter, 0);
+	}
+
+	#endregion Post-Processing and Effects
+
+	#region Audio
+
+	public void UpdateSceneMasterVolume(float percentage)
+	{
+		masterVolume = Mathf.Lerp(-80f, 0f, percentage);
+		mixer.SetFloat(MASTER_VOLUME, masterVolume);
+	}
+
+	public void UpdateSceneMusicVolume(float percentage)
+	{
+		musicVolume = Mathf.Lerp(-80f, 0f, percentage);
+		mixer.SetFloat(MUSIC_VOLUME, musicVolume);
+	}
+
+	public void UpdateSceneMusicLowPass(float percentage)
+	{
+		musicLowPass = Mathf.Lerp(800f, 22000f, percentage);
+		mixer.SetFloat(MUSIC_LOWPASS, musicLowPass);
+	}
+
+	#endregion Audio
+
+	#region Scene Loading
+
+	public void ReloadScene()
+	{
+		if (loading == null)
+		{
+			loading = StartCoroutine(LoadSceneCore(
+
+			content: () =>
+			{
+				SceneLoader.Reload();
+			}));
+		}
+	}
+
+	public void LoadNextScene()
+	{
+		if (loading == null)
+		{
+			loading = StartCoroutine(LoadSceneCore(
+
+			content: () =>
+			{
+				SceneLoader.Next();
+			}));
+		}
+	}
+
+	public void LoadMenu()
+	{
+		if (loading == null)
+		{
+			loading = StartCoroutine(LoadSceneCore(
+
+			content: () =>
+			{
+				SceneLoader.Load(Constants.MENU_SCENE);
+			}));
+		}
+	}
+
+	public void LoadSceneByName(string sceneName)
+	{
+		if (loading == null)
+		{
+			loading = StartCoroutine(LoadSceneCore(
+
+			content: () =>
+			{
+				SceneLoader.Load(sceneName);
+			}));
+		}
+	}
+
+	public void LoadSceneTransition(SceneLoading loading)
+	{
+		if (this.loading == null)
+		{
+			this.loading = StartCoroutine(LoadSceneCore(
+
+			content: () =>
+			{
+				SceneLoader.Load(loading);
+			}));
+		}
+	}
+
+	public void Quit()
+	{
+		if (loading == null)
+		{
+			loading = StartCoroutine(LoadSceneCore(
+
+			content: () =>
+			{
+				SceneLoader.Quit();
+			}));
+		}
+	}
+
+	private IEnumerator LoadSceneCore(Action content = null)
+	{
+		if (inversingColor != null)
+		{
+			StopCoroutine(inversingColor);
+		}
+
+		Time.timeScale = 1f;
+
+		if (fader != null)
+		{
+			yield return fader.FadeOutCore();
+		}
+
+		content?.Invoke();
+	}
+
+	#endregion Scene Loading
+}
---
+++ Assets/Scripts/Utils/LeaderBoard/LeaderBoardRequests.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Utils
+{
+	public static class LeaderBoardRequests
+	{
+		private static string URL = "https://mahjoub.xyz/";
+
+		public static void Get(Action<List<LeaderBoardEntry>> callback = null)
+		{
+			RoutinePool.Run(GetCore(callback));
+		}
+
+		public static void Post(string name, float value, Action<string> callback = null)
+		{
+			RoutinePool.Run(PostCore(name, value, callback));
+		}
+
+		private static IEnumerator GetCore(Action<List<LeaderBoardEntry>> callback = null)
+		{
+			var url = $"{URL}getLeaderBoard.php";
+
+			WWWForm form = new WWWForm();
+			form.AddField("game", Application.productName);
+
+			using (UnityWebRequest webRequest = UnityWebRequest.Post(url, form))
+			{
+				yield return webRequest.SendWebRequest();
+				var result = DoValidation(url, webRequest);
+
+				// Parsing retrieved data from server
+				List<LeaderBoardEntry> entries = new List<LeaderBoardEntry>();
+				foreach (var entry in result.Split('|'))
+				{
+					string[] p = entry.Split(',');
+					if (p.Length != 3) continue;
+
+					entries.Add(new LeaderBoardEntry(p[0], float.Parse(p[1]), long.Parse(p[2])));
+				}
+
+				callback?.Invoke(entries);
+			}
+		}
+
+		private static IEnumerator PostCore(string name, float value, Action<string> callback = null)
+		{
+			var url = $"{URL}addLeaderBoardEntry.php";
+
+			// Cleanup name parameter for avoiding potential content parsing issues
+			name = name.Replace("|", "").Replace(",", "");
+
+			WWWForm form = new WWWForm();
+			form.AddField("game", Application.productName);
+			form.AddField("name", name);
+			form.AddField("value", value.ToString());
+			form.AddField("utx", TimeManager.UnixTimeNow().ToString());
+
+			using (UnityWebRequest webRequest = UnityWebRequest.Post(url, form))
+			{
+				yield return webRequest.SendWebRequest();
+				var result = DoValidation(url, webRequest);
+				callback?.Invoke(result);
+			}
+		}
+
+		private static string DoValidation(string url, UnityWebRequest webRequest)
+		{
+			string[] pages = url.Split('/');
+			int page = pages.Length - 1;
+
+			switch (webRequest.result)
+			{
+				case UnityWebRequest.Result.DataProcessingError:
+					Debug.LogError(pages[page] + ": Error: " + webRequest.error);
+					break;
+
+				case UnityWebRequest.Result.ProtocolError:
+					Debug.LogError(pages[page] + ": HTTP Error: " + webRequest.error);
+					break;
+
+				case UnityWebRequest.Result.Success:
+					Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+					return webRequest.downloadHandler.text;
+			}
+
+			return null;
+		}
+	}
+}
